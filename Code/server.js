33bdc48a2e0d61f4d819010780d610c51bc1493d
R <hariprasauth.r@sap.com>
2017-07-12 15:31:57 +0530

    Initial commit

---
+++ Code/server.js
@@ -0,0 +1,21 @@
+http  = require("http")
+cfenv = require("cfenv")
+
+// get environmental information for this app
+appEnv   = cfenv.getAppEnv()
+instance = appEnv.app.instance_index || 0
+
+// create a server with a simple request handler
+server = http.createServer(onRequest)
+
+// start the server on the calculated port and host
+server.listen(appEnv.port, function() {
+    console.log("server starting on " + appEnv.url)
+})
+
+//-----------------------------------------------
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+
+  response.end("Hello World!\nNodejs version " + process.version )
+}
